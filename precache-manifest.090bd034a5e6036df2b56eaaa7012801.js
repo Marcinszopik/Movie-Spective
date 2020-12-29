0e7e79bc09cc5505d96cbc8bd90c885b7b76e14d
szopiks@yahoo.com <szopiks@yahoo.com>
2020-12-28 16:42:50 -0800

    Updates

---
+++ precache-manifest.090bd034a5e6036df2b56eaaa7012801.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f067cfa50844cb4e87bcd154ced26c5",
+    "url": "/Movie-Spective/index.html"
+  },
+  {
+    "revision": "aac513febfc910649b9b",
+    "url": "/Movie-Spective/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "8e7f68a171787985edaa",
+    "url": "/Movie-Spective/static/css/main.c562995b.chunk.css"
+  },
+  {
+    "revision": "aac513febfc910649b9b",
+    "url": "/Movie-Spective/static/js/2.14c21f12.chunk.js"
+  },
+  {
+    "revision": "8e7f68a171787985edaa",
+    "url": "/Movie-Spective/static/js/main.413933e0.chunk.js"
+  },
+  {
+    "revision": "96e30d741b9d3ce9074f",
+    "url": "/Movie-Spective/static/js/runtime-main.a0560a69.js"
+  },
+  {
+    "revision": "ea872ff0d46d13eea232953664edc01d",
+    "url": "/Movie-Spective/static/media/IndubitablyNF.ea872ff0.ttf"
+  },
+  {
+    "revision": "985589899c4bf9282be466bab05a0a94",
+    "url": "/Movie-Spective/static/media/projector-1.98558989.jpg"
+  }
+]);
